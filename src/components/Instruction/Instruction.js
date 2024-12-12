df74d0f7ba19dd5d6a3afbf2c283bb7bec3e2190
Bill <szbillyang@gmail.com>
2024-12-12 00:20:12 -0500

    fix typo

--- src/components/Instruction/Instruction.js
+++ src/components/Instruction/Instruction.js
@@ -19,7 +19,7 @@ const Instruction = () => {
       <h2>Welcome to Face Recognition App!</h2>
       <p>Follow these steps to get started:</p>
       <ol>
-        <li>Sign in as guset or register to save your entries.</li>
+        <li>Sign in as guest or register to save your entries.</li>
         <li>Find an image that contain faces and copy the link.</li>
         <li>Enter an image URL into the input box.</li>
         <li>Click the <strong>Detect</strong> button to recognize faces.</li>
